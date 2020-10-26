354c540388cfc0618ab83682b3fa431e40f2bb5e
unknown <zolotorevskii@g.nsu.ru>
2020-10-26 12:15:09 +0700

    lab1-1, first version

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -1,5 +1,68 @@
-#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#pragma warning(disable : 4996)
 
-int main(void) {
-    return EXIT_SUCCESS;
+
+int main() {
+	FILE* in = fopen("in.txt", "rt");
+	if (in == NULL) return 0;
+	char template[17];
+	int length_template = 0;
+	int ht = 0; //hash template
+	long long int power = 1;
+	for (int i = 0; i < 17; i++) {
+		if (fscanf(in, "%c", &template[i]) == 0) {
+			fclose(in);
+			return 0;
+		}
+		if (template[i] == '\n') {
+			template[i] = '\0';
+			break;
+		}
+		template[i] = template[i] % 3;
+		ht += template[i] * power;
+		power *= 3;
+		length_template += 1;
+	}
+	printf("%d ", ht);
+
+	char text[17];
+	int hs = 0;
+	power = 1;
+	int number = length_template;
+	for (int i = 0; i < length_template; i++) {
+		if (fscanf(in, "%c", &text[i]) == EOF) {
+			fclose(in);
+			return 0;
+		}
+		text[i] = text[i] % 3;
+		hs += text[i] * power;
+		power *= 3;
+	}
+	power /= 3;
+
+	if (hs == ht) {
+		for (int i = length_template - 1; i >= 0; i--) {
+			printf("%d ", number - i);
+		}
+	}
+	char input_char;
+	while (fscanf(in, "%c", &input_char) != EOF) {
+		input_char = input_char % 3;
+		char first = text[number % length_template];
+		hs = (hs - first)/3 + power * input_char;
+		text[number % length_template] = input_char;
+		number += 1;
+		if (hs == ht) {
+			for (int i = length_template -1; i >= 0; i--) {
+				printf("%d ", number - i);
+			}
+		}
+		//printf("%d ", hs);
+	}
+
+
+	int last_symbol = length_template - 1;
+	fclose(in);
+	return 0;
 }
