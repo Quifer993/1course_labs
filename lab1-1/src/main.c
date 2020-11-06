748c8b71d35d8f0419914d05f8d3fbaef79f91be
unknown <zolotorevskii@g.nsu.ru>
2020-11-06 12:10:28 +0700

    add function 'make hash' and fix style

--- lab1-1/src/main.c
+++ lab1-1/src/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
+unsigned int make_hash(unsigned char text[], long long int *power, int length) {
+	unsigned int hash = 0;
+
+	for (int i = 0; i < length; i++) {
+		hash += (text[i] % 3) * *power;
+		*power *= 3;
+	}
+	*power /= 3;
+	return hash;
+}
 
 int print_result(int number, int length_template, unsigned char* template, unsigned char* text) {
 	for (int i = 0; i < length_template; i++) {
@@ -13,15 +23,15 @@ int print_result(int number, int length_template, unsigned char* template, unsig
 	return 0;
 }
 
+
 int main() {
 	FILE* in = fopen("in.txt", "rt");
 	if (in == NULL) return 0;
+
 	unsigned char template[17];
 	int length_template = 0;
-	int ht = 0; //hash template
-	long long int power = 1;
 	for (int i = 0; i < 17; i++) {
-		if (fscanf(in, "%c", &template[i]) == 0) {
+		if (fscanf(in, "%c", &template[i]) == EOF) {
 			fclose(in);
 			return 0;
 		}
@@ -29,31 +39,28 @@ int main() {
 			template[i] = '\0';
 			break;
 		}
-		ht += ( template[i] % 3 ) * power;
-		power *= 3;
 		length_template += 1;
 	}
+
+	long long int power = 1;
+	const unsigned int ht = make_hash(template, &power, length_template);//hash template
 	printf("%d ", ht);
 
-	unsigned char text[17];
-	int hs = 0; //hash string
-	power = 1;
+	unsigned char text[17]; 
 	int number = length_template;
 	for (int i = 0; i < length_template; i++) {
 		if (fscanf(in, "%c", &text[i]) == EOF) {
 			fclose(in);
 			return 0;
 		}
-		hs += (text[i] % 3) * power;
-		power *= 3;
 	}
-	power /= 3;
-
+	
+	power = 1;
+	unsigned int hs = make_hash(text, &power, length_template); //hash string
 
 	if (hs == ht) {
 		print_result(number, length_template, template, text);
 	}
-
 	while (fscanf(in, "%c", &text[number % length_template]) != EOF) {
 		hs = hs / 3 + power * (text[number % length_template] % 3 );
 		number += 1;
