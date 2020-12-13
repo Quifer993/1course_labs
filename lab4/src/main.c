b0eaa563fbe916ee3f81f9c28ee750fa00d1cc9e
a.zolotorevskii <a.zolotorevskii@g.nsu.ru>
2020-12-13 18:40:27 +0700

    add condition i != 0 because stack-underflow

--- lab4/src/main.c
+++ lab4/src/main.c
@@ -105,7 +105,7 @@ int calc(char input[], int end, int arr_num[], char arr_zn[]) {
 						arr_zn_i += 1;
 					}
 					else {
-						if (input[i] == ')' && input[i - 1] != '(') {
+						if (i != 0 && input[i] == ')' && input[i - 1] != '(') {
 							while (arr_zn[arr_zn_i - 1] != '(') {
 								if (arr_num_i > 1 && arr_zn_i > 0) {
 									arr_num[arr_num_i - 2] = operation(arr_num[arr_num_i - 2], arr_num[arr_num_i - 1], arr_zn[arr_zn_i - 1], &error);
