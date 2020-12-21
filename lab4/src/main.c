c74c2aa178cc90628e0f5d85578ab070b9547c9a
unknown <zolotorevskii@g.nsu.ru>
2020-12-21 12:10:37 +0700

    Make struct

--- lab4/src/main.c
+++ lab4/src/main.c
@@ -6,6 +6,11 @@
 
 enum TypeError { Ok, DivByZero, SynError };
 
+typedef struct {
+	int arr[1001];
+	int size;
+}STACK;
+
 int operation(int const first, int const second, int const op, int* error) {
 	if (op == '*') {
 		return first * second;
@@ -33,18 +38,24 @@ int operation(int const first, int const second, int const op, int* error) {
 	return 0;
 }
 
+void push(STACK* s_arr, int const input) {
+	s_arr->arr[s_arr->size] = input;
+	s_arr->size++;
+}
+/*
 void push(int* stack, int const input, int* arr_i) {
 	*stack = input;
 	*arr_i = *arr_i + 1;
 }
+*/
 
-void create_num(const char input[], int* i, int* arr_num_i, int arr_num[], int const end) {
+void create_num(const char input[], int* i, STACK* s_arr_num, int const end) {
 	int number = input[*i] - '0';
 	while (*i < end && isdigit(input[*i + 1])) {
 		number = number * 10 + input[*i + 1] - '0';
 		*i = *i + 1;
 	}
-	push(&arr_num[*arr_num_i], number, arr_num_i);
+	push(s_arr_num, number);
 }
 
 int check_syn(const char input[], int const end) {
@@ -55,40 +66,33 @@ int check_syn(const char input[], int const end) {
 	int j = end - 1;
 
 	while (j >= 0) {
-		j--;
-		if (input[j + 1] == ')') {
-			count_brackets = 1;
-			break;
+
+		if (input[j] == ')') {
+			count_brackets += 1;
 		}
 
-	}
-
-	while (j >= 0 && count_brackets > 0) {
-		if (input[j] == ' ') {
-			j--;
-			continue;
+		else if (input[j] == '(' && input[j + 1] != ')') {
+			count_brackets--;
 		}
 
-		if (isdigit(input[j])) {
+		else if (input[j] == ' ') {}
+
+		else if (isdigit(input[j])) {
 			count_num++;
 
-			while (j >= 0 && isdigit(input[j + 1])) {
+			while (j > 0 && isdigit(input[j - 1])) {
 				j--;
 			}
 		}
 		else if (input[j] == '+' || input[j] == '-' || input[j] == '*' || input[j] == '/') {
 			count_zn++;
 		}
-		else if (input[j] == ')') {
-			count_brackets++;
+		else {
+			return SynError;
 		}
-		else if (input[j] == '(') {
-			count_brackets--;
-		}
-
 		j--;
 
-		if (count_num > count_zn + 1 || count_num < count_zn) {
+		if (count_num > count_zn + 1 || count_num < count_zn || count_brackets < 0) {
 			return SynError;
 		}
 	}
@@ -101,11 +105,11 @@ int check_syn(const char input[], int const end) {
 	return Ok;
 }
 
-int exe_and_pop(int arr_num[], int* arr_num_i, int arr_zn[], int* arr_zn_i) {
+int exe_and_pop(STACK* s_arr_num, STACK* s_arr_zn) {
 	int error = 0;
-	arr_num[*arr_num_i - 2] = operation(arr_num[*arr_num_i - 2], arr_num[*arr_num_i - 1], arr_zn[*arr_zn_i - 1], &error);
-	*arr_num_i -= 1;
-	*arr_zn_i -= 1;
+	s_arr_num->arr[s_arr_num->size - 2] = operation(s_arr_num->arr[s_arr_num->size - 2], s_arr_num->arr[s_arr_num->size - 1], s_arr_zn->arr[s_arr_zn->size - 1], &error);
+	s_arr_num->size -= 1;
+	s_arr_zn->size -= 1;
 	return error;
 }
 
@@ -115,6 +119,10 @@ int calc(const char input[], int const end, int* answer) {
 		return SynError;
 	}
 
+	STACK s_arr_num;
+	s_arr_num.size = 0;
+	STACK s_arr_zn;
+	s_arr_zn.size = 0;
 	int arr_num[502];
 	int arr_zn[1001];
 	int i = 0;
@@ -128,33 +136,25 @@ int calc(const char input[], int const end, int* answer) {
 		if (cur == ' ') {}
 
 		else if (isdigit(cur)) {
-			create_num(input, &i, &arr_num_i, arr_num, end);
+			create_num(input, &i, &s_arr_num, end);
 		}
 
 		else if (cur == '+' || cur == '-' || cur == '*' || cur == '/') {
 
-			if (arr_zn_i == 0) {
-				if (arr_num_i == 0 || arr_num_i >= 2) {
-					return SynError;
-				}
-
-				push(&arr_zn[0], cur, &arr_zn_i);
+			if (s_arr_zn.size == 0) {
+				push(&s_arr_zn, cur);
 			}
 
-			else if (arr_zn[arr_zn_i - 1] == '(') {
-				push(&arr_zn[arr_zn_i], cur, &arr_zn_i);
-			}
-
-			else if (arr_num_i <= 1) {
-				return SynError;
+			else if (s_arr_zn.arr[s_arr_zn.size - 1] == '(') {
+				push(&s_arr_zn, cur);
 			}
 
 			else if (cur == '*' || cur == '/') {
-				if (arr_zn[arr_zn_i - 1] == '+' || arr_zn[arr_zn_i - 1] == '-') {}
+				if (s_arr_zn.arr[s_arr_zn.size - 1] == '+' || s_arr_zn.arr[s_arr_zn.size - 1] == '-') {}
 				else {
-					error = exe_and_pop(arr_num, &arr_num_i, arr_zn, &arr_zn_i);
+					error = exe_and_pop(&s_arr_num, &s_arr_zn);
 				}
-				push(&arr_zn[arr_zn_i], cur, &arr_zn_i);
+				push(&s_arr_zn, cur);
 
 				if (error != Ok) {
 					return error;
@@ -162,68 +162,52 @@ int calc(const char input[], int const end, int* answer) {
 
 			}
 			else {
-				while (arr_zn_i > 0 && arr_zn[arr_zn_i - 1] != '(' && error == Ok) {
-					error = exe_and_pop(arr_num, &arr_num_i, arr_zn, &arr_zn_i);
+				while (s_arr_zn.size > 0 && s_arr_zn.arr[s_arr_zn.size - 1] != '(' && error == Ok) {
+					error = exe_and_pop(&s_arr_num, &s_arr_zn);
 				}
 
 				if (error != Ok) {
 					return error;
 				}
 
-				push(&arr_zn[arr_zn_i], cur, &arr_zn_i);
+				push(&s_arr_zn, cur);
 			}
 		}
 
 		else if (cur == '(') {
-			push(&arr_zn[arr_zn_i], cur, &arr_zn_i);
+			push(&s_arr_zn, cur);
 		}
 
-		else if (arr_zn_i > 0 && i > 0 && cur == ')' && input[i - 1] != '(') {
+		else if (s_arr_zn.size > 0 && i > 0 && cur == ')' && input[i - 1] != '(') {
 
-			while (arr_zn[arr_zn_i - 1] != '(' && error == Ok) {
-				if (arr_num_i > 1 && arr_zn_i > 1) {
-					error = exe_and_pop(arr_num, &arr_num_i, arr_zn, &arr_zn_i);
-				}
-				else {
-					return SynError;
-				}
+			while (s_arr_zn.arr[s_arr_zn.size - 1] != '(' && error == Ok) {
+				error = exe_and_pop(&s_arr_num, &s_arr_zn);
 			}
 
 			if (error != Ok) {
 				return error;
 			}
-
-			arr_zn_i -= 1;
+			s_arr_zn.size = s_arr_zn.size - 1;
 		}
-
-		else {
-			return SynError;
-		}
-
 		i++;
 	}
 
-	if (arr_num_i == arr_zn_i + 1) {
-		while (arr_zn_i > 0 && error == Ok) {
-			error = exe_and_pop(arr_num, &arr_num_i, arr_zn, &arr_zn_i);
-		}
-	}
-	else {
-		return SynError;
+	while (s_arr_zn.size > 0 && error == Ok) {
+		error = exe_and_pop(&s_arr_num, &s_arr_zn);
 	}
 
 	if (error != Ok) {
 		return error;
 	}
 
-	*answer = arr_num[0];
+	*answer = s_arr_num.arr[0];
 	return Ok;
 }
 
 
 int main() {
 
-	char input[1001];
+	char input[1002];
 	int length = 0;
 
 	for (int i = 0; i < 1001; i++) {
@@ -241,7 +225,7 @@ int main() {
 		printf("syntax error");
 		return 0;
 	}
-
+	input[length] = '\n';
 
 	int answer = 0;
 	int status_code = calc(input, length, &answer);
@@ -258,4 +242,3 @@ int main() {
 
 	return 0;
 }
-
