262aa5dd128c6a4e1b23ac8637318903c40f742b
Андрей <zolotorevskii@g.nsu.ru>
2021-05-04 13:35:26 +0700

    Del print in algoritm_fun, change unclear names, add type of errors

--- lab8-0/src/main.c
+++ lab8-0/src/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <limits.h>
 #include "dsu.h"
+
+
+enum TypeError { OK, LINES, VERTEX, LENGTH };
 
 
 typedef struct Edge {
@@ -13,16 +17,16 @@ typedef struct Edge {
 
 
 int compare_edges(const void* ptr_left, const void* ptr_right) {
-	int left = ((Edge*)ptr_left)->weight;
-	int right = ((Edge*)ptr_right)->weight;
+	int left = ((Edge const*)ptr_left)->weight;
+	int right = ((Edge const *)ptr_right)->weight;
 	return left - right;
 }
 
 
-int check_parents(Dsu* array, Edge* edges_ans, int n) {
+int check_parents(Dsu* dsu_vertices, Edge* edges_ans, int n) {
 	int parents = 0;
 	for (int i = 0; i < n; i++) {
-		if (array[i].parent == i) {
+		if (dsu_vertices[i].parent == i) {
 			parents++;
 		}
 
@@ -30,41 +34,30 @@ int check_parents(Dsu* array, Edge* edges_ans, int n) {
 			return parents;
 		}
 	}
-	for (int i = 0; i < n - 1; i++) {
-		printf("%i %i\n", edges_ans[i].from, edges_ans[i].to);
-	}
 	return 1;
 }
 
 
 int pull_edges(Edge *edges, int m, int n) {
-	int counter_strings = 0;
-
 	for (int i = 0; i < m; i++) {
 		if (scanf("%i%i%i", &edges[i].from, &edges[i].to, &edges[i].weight) == EOF) {
-			if (counter_strings < m) {
-				printf("bad number of lines");
-			}
-			return -1;
+			return LINES;
 		}
-		counter_strings++;
 		if (edges[i].from > n || edges[i].to > n || edges[i].from < 1 || edges[i].from >5000 || edges[i].to > 5000 || edges[i].to < 1) {
-			printf("bad vertex");
-			return -1;
+			return VERTEX;
 		}
 		if (edges[i].weight > INT_MAX || edges[i].weight < 0) {
-			printf("bad length");
-			return -1;
+			return LENGTH;
 		}
 		edges[i].from -= 1;
 		edges[i].to -= 1;
 	}
 
-	return 0;
+	return OK;
 }
 
 
-void alg_kraskal(Dsu* dsu_vertices, Edge* edges, int n, int m) {
+Edge* alg_kraskal(Dsu* dsu_vertices, Edge* edges, int n, int m) {
 	qsort(edges, m, sizeof(Edge), compare_edges);
 	int edges_count = 0;
 
@@ -75,15 +68,17 @@ void alg_kraskal(Dsu* dsu_vertices, Edge* edges, int n, int m) {
 
 	for (int i = 0; i < m; i++) {
 		if (dsu_find_parents(dsu_vertices, edges[i].from, edges[i].to)) {
-			edges_ans[edges_count].from = edges[i].from + 1;
-			edges_ans[edges_count++].to = edges[i].to + 1;
+			edges_ans[edges_count].from = edges[i].from;
+			edges_ans[edges_count++].to = edges[i].to;
 		}
 	}
 
 	if (check_parents(dsu_vertices, edges_ans, n) != 1) {
-		printf("no spanning tree");
+		free(edges_ans);
+		edges_ans = NULL;
 	}
-	free(edges_ans);
+
+	return edges_ans;
 }
 
 
@@ -125,8 +120,27 @@ int main() {
 		dsu_vertices[i].parent= i;
 	}
 
-	if (pull_edges(edges, m, n) != -1) {
-		alg_kraskal(dsu_vertices, edges, n, m);
+	int is_error = pull_edges(edges, m, n);
+	if (is_error == OK) {
+		Edge* edges_answer = alg_kraskal(dsu_vertices, edges, n, m);
+		if (edges_answer == NULL) {
+			printf("no spanning tree");
+		}
+		else {
+			for (int i = 0; i < n - 1; i++) {
+				printf("%i %i\n", edges_answer[i].from + 1, edges_answer[i].to + 1);
+			}
+			free(edges_answer);
+		}
+	}
+	else if(is_error ==	LINES){
+		printf("bad number of lines");
+	}
+	else if(is_error == VERTEX){
+		printf("bad vertex");
+	}
+	else if(is_error == LENGTH){
+		printf("bad length");
 	}
 
 	free(dsu_vertices);
